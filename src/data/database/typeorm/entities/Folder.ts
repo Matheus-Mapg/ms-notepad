40b65b9144f5cd28e993d7f591da8e18436c1ad9
Matheus-Mapg <mapg.goes@gmail.com>
2023-01-07 23:31:06 -0300

    :card_file_box: mudança na estrutura das tabelas

--- src/data/database/typeorm/entities/Folder.ts
+++ src/data/database/typeorm/entities/Folder.ts
@@ -1,17 +1,30 @@
-import { Column, Entity, Index, OneToMany } from "typeorm";
+import { Column, Entity, OneToMany } from "typeorm";
 import { NotepadFolder } from "./NotepadFolder";
 
-@Index("folder_NAME_IDX", ["name"], { unique: true })
 @Entity("folder", { schema: "ms-notepad" })
 export class Folder {
   @Column("varchar", { primary: true, name: "ID", length: 255 })
   id: string;
 
-  @Column("varchar", { name: "NAME", unique: true, length: 30 })
+  @Column("varchar", { name: "NAME", length: 30 })
   name: string;
 
   @Column("varchar", { name: "DESCRIPTION", nullable: true, length: 100 })
   description: string | null;
+
+  @Column("datetime", {
+    name: "CREATE_AT",
+    nullable: true,
+    default: () => "CURRENT_TIMESTAMP",
+  })
+  createAt: Date | null;
+
+  @Column("datetime", {
+    name: "UPDATE_AT",
+    nullable: true,
+    default: () => "CURRENT_TIMESTAMP",
+  })
+  updateAt: Date | null;
 
   @OneToMany(() => NotepadFolder, (notepadFolder) => notepadFolder.idFolder2)
   notepadFolders: NotepadFolder[];
--- src/data/database/typeorm/entities/Notepad.ts
+++ src/data/database/typeorm/entities/Notepad.ts
@@ -1,17 +1,30 @@
-import { Column, Entity, Index, OneToMany } from "typeorm";
+import { Column, Entity, OneToMany } from "typeorm";
 import { NotepadFolder } from "./NotepadFolder";
 
-@Index("notepad_NAME_IDX", ["name"], { unique: true })
 @Entity("notepad", { schema: "ms-notepad" })
 export class Notepad {
   @Column("varchar", { primary: true, name: "ID", length: 255 })
   id: string;
 
-  @Column("varchar", { name: "NAME", unique: true, length: 30 })
+  @Column("varchar", { name: "NAME", length: 30 })
   name: string;
 
   @Column("varchar", { name: "NOTE", nullable: true, length: 100 })
   note: string | null;
+
+  @Column("datetime", {
+    name: "CREATE_AT",
+    nullable: true,
+    default: () => "CURRENT_TIMESTAMP",
+  })
+  createAt: Date | null;
+
+  @Column("datetime", {
+    name: "UPDATE_AT",
+    nullable: true,
+    default: () => "CURRENT_TIMESTAMP",
+  })
+  updateAt: Date | null;
 
   @OneToMany(() => NotepadFolder, (notepadFolder) => notepadFolder.idNote2)
   notepadFolders: NotepadFolder[];
